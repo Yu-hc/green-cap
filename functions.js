6ab12d5892794ac7327a16a6abbd4883ecb5fd52
Ethan Yu <56643584+qwerqazwsx@users.noreply.github.com>
2024-06-11 13:54:00 +0800

    basic structure with js available

---
+++ functions.js
@@ -0,0 +1,9 @@
+const electron = require('electron')
+
+// Importing BrowserWindow from Main Process using Electron remote
+const BrowserWindow = electron.remote.BrowserWindow;
+
+function changeColor(newColor) {
+	const elem = document.getElementById("para");
+	elem.style.color = newColor;
+ }
